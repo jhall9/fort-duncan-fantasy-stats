cf72f39dad9ca8a94de97375adbf8830f9b72749
jhall9 <jeromephall@gmail.com>
2025-12-17 08:46:00 -0600

    Update next.config.js to reference repo base path

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   reactStrictMode: true,
+  // Set basePath for GitHub Pages deployment
+  basePath:
+    process.env.NODE_ENV === "production" ? "/fort-duncan-fantasy-stats" : "",
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "/fort-duncan-fantasy-stats/" : "",
   // Disable image optimization for static export
   images: {
     unoptimized: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
